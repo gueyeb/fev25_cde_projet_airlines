c960d41c0931a84b8cd1a4610111fd895158f461
Babacar Gueye <gueyens11@gmail.com>
2025-06-27 05:21:12 +0200

    update path import for historical_flights script

--- sql/historical_flights.py
+++ sql/historical_flights.py
@@ -1,4 +1,6 @@
-
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from functions.pg_functions import build_historical_flights_from_bts
 
 if __name__ == "__main__":
